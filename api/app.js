0d89d820aacbda3d630416bb705a9857766eb4c4
Jean Cremonezzi <jeancremonezzidev@gmail.com>
2024-02-25 15:01:18 -0300

    feat: Add error handling

--- api/app.js
+++ api/app.js
@@ -11,6 +11,11 @@ app.listen('3000', () => {
    console.log(`\nApp running on port 3000\n`)
 })
 
+// ROUTE TO GET PRODUCTS DATA
 app.get('/api/scrape', async (req, res) => {
-   res.json(await fetchData(req.query.keyword))
+   try {
+      res.json(await fetchData(req.query.keyword))
+   } catch ({response}) {
+      res.sendStatus(response.status)
+   }
 })
--- api/utils/dataFetcher.js
+++ api/utils/dataFetcher.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 
+// Fecth data from amazon. Selects title, rating, reviews, image, price and link from products
 const fetchData = async (keyword) => {
    return await axios.get(`https://www.amazon.com/s?k=${keyword}`, {
       headers: {
@@ -11,15 +12,18 @@ const fetchData = async (keyword) => {
       const $ = cheerio.load(data);
       const products = []
 
+      // Iterates through found products and gathers data
       $('[data-component-type=s-search-result]').each((_idx, el) => {
          const row = $(el)
          let data = {}
 
+         // REQUIRED DATA
          data["title"] = row.find('h2').text().trim()
          data["rating"] = row.find('span.a-icon-alt').html()
          data["reviews"] = row.find('[data-component-type=s-client-side-analytics]').text().trim()
          data["img"] = row.find('img.s-image').attr("src")
 
+         // ADDITIONAL DATA
          data["price"] = row.find('span.a-price-whole').text() + row.find('span.a-price-fraction').text().trim() || undefined
          data["link"] = `https://www.amazon.com${row.find('h2').find('a').attr("href")}`
 
